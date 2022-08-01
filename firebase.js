6d2d4e9f5d9a38ce0ebd12ff2faf37e76c9ca600
RobinSrk <robinsrk3@gmail.com>
2022-08-01 22:45:17 +0600

    updated firebase config

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBwHMEg43CWLbRg7kfsUt3PjwaLMjEbjIw",
   authDomain: "robmess-b65d6.firebaseapp.com",
